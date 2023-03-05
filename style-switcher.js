e96497f0b8813eb5b6d94817dcb0b6e55aed8e67
shirone Fortuno <118256204+shironeF@users.noreply.github.com>
2023-03-06 05:37:49 +0800

    adding nigth mode

---
+++ style-switcher.js
@@ -0,0 +1,28 @@
+const stylSwitcherToggle = document.querySelector(".style-switcher-toggler");
+stylSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+
+window,addEventListener("scroll", () => {
+    if(document.querySelector(".style-switcher").classList.contains("open"))
+    {
+        document.querySelector(".style-switcher").classList.remove("open");
+    } 
+})
+
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color)
+{
+    alternateStyles.forEach((style) => {
+        if(color === style.getAttribute("title"))
+        {
+            style.removeAttribute("disabled");
+        }
+        else
+        {
+            style.setAttribute("disabled", "true");
+        }
+    })
+}
+
+const dayNight = document.querySelector(".day-night");
