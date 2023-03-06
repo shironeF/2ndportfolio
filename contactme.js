95b6a8f8a1f3b80de01c0fb58e7831abb5f1be8b
shirone Fortuno <118256204+shironeF@users.noreply.github.com>
2023-03-07 00:53:10 +0800

    added sent message function

---
+++ contactme.js
@@ -0,0 +1,16 @@
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwmWd3OAttXudsyWQ7yx1SaVwoM1LeizeHfhNBr9KV0bKHt_uSYiyP-bEu7LscsYgX8fQ/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message Sent Successfully, Thank You!"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+    })
+      .catch(error => console.error('Error!', error.message))
+  })
