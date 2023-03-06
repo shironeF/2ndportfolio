9fafbcfa492c394dacb43184535a781f512debe3
shirone Fortuno <118256204+shironeF@users.noreply.github.com>
2023-03-06 08:21:32 +0800

    added animations for project section

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".typing", {
+    strings:["", "Web Designer", "UI/UX Designer", "Web Developer", "Software Engineer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+const nav = document.querySelector()
