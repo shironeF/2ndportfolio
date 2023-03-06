d152df2a0e04a3816b0f24c87156ec5d7814ddec
shirone Fortuno <118256204+shironeF@users.noreply.github.com>
2023-03-07 04:46:46 +0800

    change . in style js

--- style-switcher.js
+++ style-switcher.js
@@ -3,7 +3,7 @@ stylSwitcherToggle.addEventListener("click", () => {
     document.querySelector(".style-switcher").classList.toggle("open");
 })
 
-window,addEventListener("scroll", () => {
+window.addEventListener("scroll", () => {
     if(document.querySelector(".style-switcher").classList.contains("open"))
     {
         document.querySelector(".style-switcher").classList.remove("open");
